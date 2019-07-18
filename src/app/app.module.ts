b0fc1de2213242231827be8d88a813521c852313
venuschau <Venus.Chau@pccw.com>
2019-07-18 12:29:57 +0800

    create subject env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubjectComponent } from './components/subject/subject/subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectComponent
   ],
   imports: [
     BrowserModule,

7bfbe15cc253500b85efdcdf81813fa2466fde71
venuschau <Venus.Chau@pccw.com>
2019-07-18 16:50:15 +0800

    map successfully

--- src/app/components/take/take.component.ts
+++ src/app/components/take/take.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, map } from 'rxjs/operators';
 // import 'rxjs/add/operator/';
 
 
@@ -16,7 +16,8 @@ export class TakeComponent implements OnInit {
   ngOnInit() {
     const intervalCount = interval(1000);
     const takeFive = intervalCount.pipe(take(5));
-    takeFive.subscribe(x => console.log(x));
+
+    takeFive.pipe(map(x => x*10)).subscribe(x => console.log(x));
   }
 
   ngOnDestroy() {
